10f445c7d336638e7bc281980c17e986e310c0bc
Fani Casco <fanymar@hotmail.com>
2023-09-15 20:05:17 -0300

    Calculadora hecha con Js para trabajo práctico de React

--- calculadora.js
+++ calculadora.js
@@ -13,7 +13,7 @@ botones.forEach(boton => {
 
         if (boton.id === "borrar") {
             if (pantalla.textContent.length === 1 || pantalla.textContent === "Error") {
-                pantalla.textContent = "0"
+                pantalla.textContent = "0";
             } else {
 
                 pantalla.textContent = pantalla.textContent.slice(0, -1);
